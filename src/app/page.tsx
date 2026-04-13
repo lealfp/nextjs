bcbcb6af781503e03973591abffaf2fc8a2276e6
lealfp <pantoja.ti@gmail.com>
2026-04-13 19:28:00 -0300

    Alterando layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,183 +1,177 @@
+// "use client";
 import { getPosts } from "@/actions/post.action";
 import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
+import Sidebar from "@/components/Sidebar";
+import SidebarHome from "@/components/SidebarHome";
+import Cards from "@/components/Cards";
+import Accordion from "@/components/Accordion";
 
 export default async function Home() {
+  // const [showPortfolio, setShowPortfolio] = useState(true);
   const user = await currentUser();
-  const posts = await getPosts();
+  // const posts = await getPosts();
   const dbUserId = await getDbUserId();
 
-  const projects = [
-    {
-      title: "Rede Social",
-      description:
-        "Rede social com autenticação de usuários, criação e interação em publicações, sistema de seguidores.",
-      tech: ["Next.js", "Prisma", "PostgreSQL"],
-      link: "redesocial",
-    },
-    {
-      title: "Sistema SaaS Educacional",
-      description:
-        "Plataforma web para gestão acadêmica com autenticação, controle de acesso e pagamentos.",
-      tech: ["Next.js", "Prisma", "PostgreSQL"],
-      link: "#",
-    },
-    {
-      title: "Loja online.",
-      description: "Serviço eCommerce utilizando Shopify",
-      tech: ["Shopify", "TypeScript", "Docker"],
-      link: "#",
-    },
-    {
-      title: "Controle de Acesso via catraca eletrônica",
-      description: "Serviço eCommerce utilizando Shopify",
-      tech: ["SIG-UFRN", "Arduino", "Java", "MicroServiços"],
-      link: "#",
-    },
-    // {
-    //   title: "Plataforma de Conteúdo Científico",
-    //   description:
-    //     "Sistema para publicação e organização de artigos acadêmicos.",
-    //   tech: ["Next.js", "Tailwind", "Clerk"],
-    //   link: "#",
-    // },
-  ];
-
   return (
-    <main className="min-h-screen bg-gray-50 text-gray-900">
+    <main>
+      <header className="w-full border-b border-gray-200 bg-primary">
+        <div className="max-w-7xl mx-auto px-4 py-6">
+          <section className="flex items-center justify-between">
+            <h1 className="text-2xl md:text-3xl font-semibold text-primary-foreground">
+              Fagner Leal Pantoja
+            </h1>
+          </section>
+
+          {/* DIVISOR */}
+          <div className="my-4 h-px bg-[#085c77]" />
+
+          {/* ID SECTION */}
+          <section className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+            {/* ESQUERDA */}
+            <div className="flex items-center gap-3">
+              {/*<img src="/orcid-logo.svg" alt="orcid" className="w-8 h-8" />*/}
+
+              <h2 className="text-gray-700 text-sm md:text-base text-primary-foreground">
+                https://orcid.org/0000-0002-3928-5432
+              </h2>
+            </div>
+          </section>
+        </div>
+      </header>
       {/* Hero */}
-      <section className="max-w-6xl mx-auto px-6 py-20 text-center">
-        <h1 className="text-5xl font-bold mb-6">Fagner Leal Pantoja</h1>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
-          Desenvolvedor Fullstack
-        </p>
+      {/*<aside className="sidebar">
+        <img src="/profile.jpg" className="avatar" />
 
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left w-[150%]">
-          Habilidades em Java, JavaScript, Node.js, React, Postgres, Docker,
-          Linux
-        </p>
+        <h2>Fagner Leal</h2>
 
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
-          Especialista em Sistemas Web
-        </p>
+        <p className="orcid">ORCID: 0000-0000-0000-0000</p>
 
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
-          Arquitetura e Projetos de Software
-        </p>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
-          Mestre/Doutor em Ciência da Computação
-        </p>
-      </section>
-      {/* Projetos */}
-      <section className="max-w-6xl mx-auto px-6 pb-20">
-        <h2 className="text-3xl font-semibold mb-12 text-center">Portfólio</h2>
-
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
-            <div
-              key={index}
-              className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition"
-            >
-              <h3 className="text-xl font-semibold mb-3">{project.title}</h3>
-
-              <p className="text-gray-600 mb-4">{project.description}</p>
-
-              <div className="flex flex-wrap gap-2 mb-4">
-                {project.tech.map((tech, i) => (
-                  <span
-                    key={i}
-                    className="bg-gray-100 text-sm px-3 py-1 rounded-full"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <a
-                href={project.link}
-                className="text-blue-600 font-medium hover:underline"
-              >
-                Ver projeto →
-              </a>
-            </div>
-          ))}
+        <div className="links">
+          <a href="#">Google Scholar</a>
+          <a href="#">GitHub</a>
+          <a href="#">LinkedIn</a>
         </div>
-      </section>
-      {/* Projetos */}
-      <section className="max-w-6xl mx-auto px-6 pb-20">
-        <h2 className="text-3xl font-semibold mb-12 text-center">Acadêmico</h2>
-      </section>
-
-      <section className="max-w-6xl mx-auto px-6 py-20 text-left">
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-          Processsamento de Linguagem Natural
-        </p>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-          Modelos de Linguagem
-        </p>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-          Inteligência Artificial
-        </p>
-      </section>
-      {/* Projetos */}
-      {/*<section className="max-w-6xl mx-auto px-6 pb-20">
-        <h2 className="text-3xl font-semibold mb-12 text-center">Projetos</h2>
-
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
-            <div
-              key={index}
-              className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition"
-            >
-              <h3 className="text-xl font-semibold mb-3">{project.title}</h3>
-
-              <p className="text-gray-600 mb-4">{project.description}</p>
-
-              <div className="flex flex-wrap gap-2 mb-4">
-                {project.tech.map((tech, i) => (
-                  <span
-                    key={i}
-                    className="bg-gray-100 text-sm px-3 py-1 rounded-full"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <a
-                href={project.link}
-                className="text-blue-600 font-medium hover:underline"
-              >
-                Ver projeto →
-              </a>
+      </aside>
+      */}
+      <div className="py-8">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+            <div className="hidden lg:block lg:col-span-3">
+              {/*<Sidebar />*/}
+              <SidebarHome />
             </div>
-          ))}
+
+            {/*<Cards />*/}
+
+            <div className="lg:col-span-9">
+              {/* Projetos */}
+
+              {/*<button
+                onClick={() => setShowPortfolio(!showPortfolio)}
+                className="mb-6 px-4 py-2 bg-primary text-primary-foreground rounded-lg"
+              >
+                {showPortfolio ? "Esconder Portfólio" : "Mostrar Portfólio"}
+              </button>*/}
+
+              {/*<section className="max-w-6xl mx-auto px-6 pb-20">*/}
+              <section className="max-w-3xl mx-auto py-10">
+                <h3 className="text-xl font-semibold text-3xl font-semibold">
+                  Biography
+                </h3>
+                <Accordion />
+                {/*<section className="max-w-6xl mx-auto px-6 py-20 text-center">*/}
+                {/*<h1 className="text-5xl font-bold mb-6">Expertise</h1>*/}
+                {/*<h2 className="text-3xl font-semibold mb-12 text-center">*/}
+                <h3 className="text-xl font-semibold text-3xl font-semibold">
+                  Expertise
+                </h3>
+
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                  Fullstack Developer
+                </p>
+
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left w-[150%]">
+                  Habilidades em Java, JavaScript, Node.js, React, Postgres,
+                  Docker, Linux
+                </p>
+
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                  Especialista em Sistemas Web
+                </p>
+
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                  Arquitetura e Projetos de Software
+                </p>
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                  Mestre/Doutor em Ciência da Computação
+                </p>
+              </section>
+
+              {/*<section className="max-w-6xl mx-auto px-6 pb-20">*/}
+              <section className="max-w-3xl mx-auto py-10">
+                {/*<h2 className="text-3xl font-semibold mb-12 text-center">*/}
+                <h3 className="text-xl font-semibold text-3xl font-semibold">
+                  Acadêmico
+                </h3>
+              </section>
+
+              {/*<p className="text-left text-muted-foreground mb-4">*/}
+              <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                Bachelor on Computer Science at UFPA
+              </p>
+
+              {/*<p className="text-left text-muted-foreground mb-4">*/}
+              <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                Master on Computer Science at Unicamp
+              </p>
+
+              <p className="text-xl text-gray-600 max-w-2xl mx-auto text-left">
+                {/*<p className="text-left text-muted-foreground mb-4">*/}
+                Doctor on Computer Science at Unicamp
+              </p>
+
+              <section className="max-w-6xl mx-auto px-6 py-20 text-left">
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+                  Processsamento de Linguagem Natural
+                </p>
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+                  Modelos de Linguagem
+                </p>
+                <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+                  Inteligência Artificial
+                </p>
+                {/*</section>*/}
+              </section>
+
+              <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+                {/*<div className="lg:col-span-6">
+                  {user ? <CreatePost /> : null}
+
+                  <div className="space-y-6">
+                    {posts.map((post) => (
+                      <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+                    ))}
+                  </div>
+                </div>
+
+                <div className="hidden lg:block lg:col-span-4 sticky top-20">
+                  <WhoToFollow />
+                </div>*/}
+              </div>
+            </div>
+          </div>
         </div>
-      </section>*/}
-      {/* Footer */}
-      <footer className="text-center py-10 border-t text-gray-500">
+      </div>
+
+      {/* Projetos */}
+
+      <footer className="text-center py-10 border-t text-gray-500 bg-[#f5f5f5]">
         © {new Date().getFullYear()} Fagner Leal. Todos os direitos reservados.
       </footer>
     </main>
   );
-  // return (
-  //   <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-  //     <div className="lg:col-span-6">
-  //       {user ? <CreatePost /> : null}
-
-  //       <div className="space-y-6">
-  //         {posts.map((post) => (
-  //           <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-  //         ))}
-  //       </div>
-  //     </div>
-
-  //     <div className="hidden lg:block lg:col-span-4 sticky top-20">
-  //       <WhoToFollow />
-  //     </div>
-  //   </div>
-  // );
 }
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import AccordionItem from "./AccordionItem";
+
+import { useState } from "react";
+
+const projects = [
+  {
+    title: "Rede Social",
+    description:
+      "Rede social com autenticação de usuários, criação e interação em publicações, sistema de seguidores.",
+    tech: ["Next.js", "Prisma", "PostgreSQL"],
+    link: "redesocial",
+  },
+  {
+    title: "Sistema Escolar",
+    description:
+      "Plataforma web para gestão acadêmica com autenticação, controle de acesso e pagamentos.",
+    tech: ["Next.js", "Prisma", "PostgreSQL"],
+    link: "#",
+  },
+  {
+    title: "Loja online.",
+    description: "Serviço eCommerce utilizando Shopify",
+    tech: ["Shopify", "TypeScript", "Docker"],
+    // link: "#",
+  },
+  {
+    title: "Controle de Acesso via catraca eletrônica",
+    description: "Serviço eCommerce utilizando Shopify",
+    tech: ["SIG-UFRN", "Arduino", "Java", "MicroServiços"],
+    link: "#",
+  },
+];
+
+export default function Accordion() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <section className="max-w-3xl mx-auto py-10">
+      <div
+        onClick={() => setOpen(!open)}
+        className="flex justify-between items-center cursor-pointer"
+      >
+        <h3 className="text-xl font-semibold text-3xl font-semibold">
+          Portfolio <span className="text-gray-500">({projects.length})</span>
+        </h3>
+
+        {/*<h2 className="text-2xl font-bold mb-6">Portfolio</h2>*/}
+        <span className="text-xl">{open ? "−" : "+"}</span>
+      </div>
+
+      <div
+        className={`transition-all duration-300 overflow-hidden ${
+          open ? "max-h-1500 mt-4" : "max-h-0"
+        }`}
+      >
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition"
+            >
+              <h3 className="text-xl font-semibold mb-3">{project.title}</h3>
+
+              <p className="text-gray-600 mb-4">{project.description}</p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                {project.tech.map((tech, i) => (
+                  <span
+                    key={i}
+                    className="bg-gray-100 text-sm px-3 py-1 rounded-full"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <a
+                href={project.link}
+                className="text-blue-600 font-medium hover:underline"
+              >
+                Ver projeto →
+              </a>
+            </div>
+          ))}
+        </div>
+
+        {/*<p className="text-sm mb-2">
+          📍 {item.city}, {item.country}
+        </p>
+
+        {item.link && (
+          <a href={item.link} className="text-primary hover:underline text-sm">
+            Ver organização →
+          </a>
+        )}*/}
+        {/*</div>*/}
+      </div>
+    </section>
+  );
+}
