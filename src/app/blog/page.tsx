9ac37179ecf1f6dea173c942a16830c996168745
lealfp <pantoja.ti@gmail.com>
2026-05-08 02:03:26 -0300

    wip

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -132,14 +132,14 @@ export default async function BlogPage() {
           ))}
         </div>
 
-        <HeroPost
+        {/*<HeroPost
           title={heroPost.title}
           coverImage={heroPost.coverImage}
           date={heroPost.date}
           author={heroPost.author}
           slug={heroPost.slug}
           excerpt={heroPost.excerpt}
-        />
+        />*/}
 
         {/*{morePosts.length > 0 && <MoreStories posts={morePosts} />}*/}
       </Container>
